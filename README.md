e60b2ef3b22620e56dd3c3af423667382124751b
Ashraya Joshi <ashraya@fusemachines.com>
2024-08-10 09:27:45 -0400

    Naming Conventions

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 * The client can send whatever data they want
 * The data isn't getting validated
 * We ultimately want to force the client to send data in a schema that we expect
+
+
+### CRUD
+* Create: Post
+* Read: Get
+* Update: Put/Patch
+* Delete: Delete
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[float] = None
 
 @app.get("/")
 async def read_root():
@@ -11,7 +19,7 @@ async def read_root():
 def get_posts():
     return {"data": "This is your posts"}
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}. content: {payload['content']}"}
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post.model_dump())
+    return {"data": "New Post"}
